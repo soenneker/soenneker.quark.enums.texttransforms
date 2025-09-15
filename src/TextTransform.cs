00535802d78097547ad496bdebf91e6a170b107f
Jake Soenneker <jake@soenneker.com>
2025-09-15 17:17:57 -0500

    Initial commit

---
+++ src/TextTransform.cs
@@ -0,0 +1,28 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.TextTransforms;
+
+/// <summary>
+/// An HTML CSS keyword set in .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public sealed partial class TextTransform
+{
+    /// <summary>
+    /// Converts all text to lowercase.
+    /// </summary>
+    public static readonly TextTransform Lowercase = new("lowercase");
+
+    /// <summary>
+    /// Converts all text to uppercase.
+    /// </summary>
+    public static readonly TextTransform Uppercase = new("uppercase");
+
+    /// <summary>
+    /// Capitalizes the first letter of each word.
+    /// </summary>
+    public static readonly TextTransform Capitalize = new("capitalize");
+
+    public static implicit operator TextTransform(GlobalKeyword style) => new(style.Value);
+}
---
+++ test/Soenneker.Quark.Enums.TextTransforms.Tests/TextTransformTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.TextTransforms.Tests;
+
+public sealed class TextTransformTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
