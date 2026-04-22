7358c58958486df8ed27262f40c1ede9c06df4ce
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:30 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.TextTransforms.Tests/TextTransformTests.cs
+++ test/Soenneker.Quark.Enums.TextTransforms.Tests/TextTransformTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.TextTransforms.Tests;
 
 public sealed class TextTransformTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
