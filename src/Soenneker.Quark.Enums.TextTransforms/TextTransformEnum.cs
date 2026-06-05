0288258f0981c0aa368eb6ca406f2f7c87f0ec8d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:16 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.TextTransforms/TextTransformEnum.cs
+++ src/Soenneker.Quark.Enums.TextTransforms/TextTransformEnum.cs
@@ -2,10 +2,22 @@ using Soenneker.Gen.EnumValues;
 
 namespace Soenneker.Quark;
 
+/// <summary>
+/// Represents the text transform enum.
+/// </summary>
 [EnumValue<string>]
 public sealed partial class TextTransformEnum
 {
+    /// <summary>
+    /// The lowercase.
+    /// </summary>
     public static readonly TextTransformEnum Lowercase = new("lowercase");
+    /// <summary>
+    /// The uppercase.
+    /// </summary>
     public static readonly TextTransformEnum Uppercase = new("uppercase");
+    /// <summary>
+    /// The capitalize.
+    /// </summary>
     public static readonly TextTransformEnum Capitalize = new("capitalize");
 }
